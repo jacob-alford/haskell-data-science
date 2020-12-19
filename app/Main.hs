84456ed2fcfe7cb4f2795bd082f3f2947cd83190
Jacob Alford - MBP <jacob.alford@me.com>
2020-12-19 14:25:33 -0700

    why is haskell so hard

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Data.Text
 import Lib
+import qualified Network.HTTP.Req as R
 
-main :: IO ()
+main :: Text -> Text -> IO (R.JsonResponse ())
 main = getCovidData
--- src/Lib.hs
+++ src/Lib.hs
@@ -8,6 +8,7 @@ where
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Aeson (KeyValue ((.=)))
 import qualified Data.Aeson as J
+import Data.Text (Text)
 import Network.HTTP.Req ((/:))
 import qualified Network.HTTP.Req as R
 
