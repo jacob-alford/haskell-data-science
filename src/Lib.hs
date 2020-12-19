5e3ab7c4fb6f235c1d8b352a5b0e33ae77027cae
Jacob Alford - MBP <jacob.alford@me.com>
2020-12-19 13:56:54 -0700

    Holy god I just made an XML HTTP Request

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-  ( someFunc,
+  ( getCovidData,
   )
 where
 
-import qualified Network.HTTP as N
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Aeson (KeyValue ((.=)))
+import qualified Data.Aeson as J
+import Network.HTTP.Req ((/:))
+import qualified Network.HTTP.Req as R
 
-someFunc :: IO ()
-someFunc = print "Hello"
+getCovidData :: IO ()
+getCovidData = R.runReq R.defaultHttpConfig $ do
+  r <-
+    R.req
+      R.GET
+      (R.https "api.covidtracking.com" /: "v1" /: "states" /: "nm" /: "daily.json")
+      R.NoReqBody
+      R.jsonResponse
+      mempty
+  liftIO $ print (R.responseBody r :: J.Value)
